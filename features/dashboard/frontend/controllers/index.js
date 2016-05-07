bb3f3a97d841c0084567532f11557081a398cb78
Phan Duc Viet <viet3695@gmail.com>
2016-05-07 15:05:39 +0700

    up right side bar

--- features/dashboard/frontend/controllers/index.js
+++ features/dashboard/frontend/controllers/index.js
@@ -3,8 +3,22 @@
 module.exports = function (controller, component, application) {
 
     controller.index = function (req, res) {
-        res.frontend.render('index', {
-            title: 'Home page'
+        application.feature.blog.actions.findAll({
+            where: {
+                published: 1,
+                type: "post"
+            },
+            include: [
+                {
+                    model: application.models.user
+                }
+            ],
+            limit: application.getConfig("pagination").frontNumberItem | 10
+        }).then(function (posts) {
+            res.frontend.render('index', {
+                postTitle: 'Welcome To TechMaster',
+                posts: posts
+            })
         })
     };
 };
